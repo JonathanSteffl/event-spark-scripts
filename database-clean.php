5e1bc278cdf35eefb8611599dea72af53ef01133
Will Nayes <wnayes@gmail.com>
2013-09-10 13:49:50 -0500

    Removed need to clean attendance and reports due to new foreign key constraints.

--- database-clean.php
+++ database-clean.php
@@ -10,16 +10,6 @@ try {
 	$count = $dbx->exec($query);
 	echo "  Number of events removed: " . $count . PHP_EOL;
 
-	// Remove associated attendance entries
-	$query = "DELETE FROM " . $GLOBALS['attend_t'] . " WHERE id NOT IN (SELECT id FROM " . $GLOBALS['event_t'] . ")";
-	$count = $dbx->exec($query);
-	echo "  Attendance records removed: " . $count . PHP_EOL;
-
-	// Remove associated reports as the event has expired naturally.
-	$query = "DELETE FROM " . $GLOBALS['report_t'] . " WHERE id NOT IN (SELECT id FROM " . $GLOBALS['event_t'] . ")";
-	$count = $dbx->exec($query);
-	echo "  Report records removed: " . $count . PHP_EOL;
-
 	$dbx = NULL;
 	echo "Cron job finished successfully." . PHP_EOL;
 }
@@ -35,8 +25,6 @@ function getConnection() {
 
 	// Keep the table variables available globally.
 	$GLOBALS["event_t"] = $event_t;
-	$GLOBALS["attend_t"] = $attend_t;
-	$GLOBALS["report_t"] = $report_t;
 
 	$dbh = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
